20add9488f6580d326fdbc683c5dc262ae53bb9b
irina <irinawebdev@gmail.com>
2024-10-22 22:35:54 +0300

    ajax

--- public/partials/header.php
+++ public/partials/header.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- jquery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
     <title>4langs</title>
 </head>
 <body>
